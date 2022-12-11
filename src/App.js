6ced144d34199e71c663a739bbf0a6c10a432f09
NikolayS <nikolay.sedelkov@gmail.com>
2022-12-11 22:45:08 +0300

    task2.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Gallery from './Gallery.js';
 
+const products = [{
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "red",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/1.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "green",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/2.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "blue",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/3.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "black",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/4.jpg"
+}, {
+  name: "Nike free run",
+  price: "170",
+  color: "black",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/7.jpg"
+}, {
+  name: "Nike Metcon 3",
+  price: "150",
+  color: "green",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/5.jpg"
+}];
+
+var isView = true;
 function App() {
+  const [icon, setIcon] = useState("view_list");
+  const [_class, setClass] = useState("table-item");
+  const Switch = () => {
+    isView = !isView;
+    if(isView) {
+      setIcon("dashboard");
+      setClass("table-item");
+    }
+    else{
+      setIcon("view_list");
+      setClass("list-item");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="conteiner__view-button">
+        <span className="material-icons" onClick={Switch}>{icon}</span>
+      </div>
+      <Gallery data={products} _class={_class}/>
     </div>
   );
 }
